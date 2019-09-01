f9926797bd0bb5e6a6cee35238768346b5e58209
Gabby Asuncion <gabrielaasuncion@gmail.com>
2019-09-01 14:40:38 -0400

    wrap some thingies

--- HelloWorld.jsx
+++ HelloWorld.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class HelloWorld extends Component {
-    render() {
-        return (
-        	/*'TODO (HelloWorld): Add a line such that "Hello World!" is displayed on your webpage!*/
-        );
-    }
+  render() {
+    return (
+      <h1>
+        {/* 'TODO (HelloWorld): Add a line such that "Hello World!" is displayed on your webpage! */}
+      </h1>
+    );
+  }
 }
 
 export default HelloWorld;
